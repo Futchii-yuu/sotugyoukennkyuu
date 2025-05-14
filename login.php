d8c6120c6fe906b7dd6851c6ff7be61c8d05fd88
ふっちー <yukifuchiwaki@gmail.com>
2025-05-14 15:11:25 +0900

    Add files via upload

---
+++ login.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>テスト</title>
+</head>
+<body>
+    <?php
+        print_r($_POST);
+    ?>
+</body>
+</html>
